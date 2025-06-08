f9e4a435bdb30fbf111fca57660c708ae52b5c69
Tirth0003 <tirthbhinagradiya1604@gmail.com>
2025-06-08 15:27:45 +0530

    routing and request handler

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
+const express = require("express");
+const { execArgv } = require("process");
+
+const app = express();
+
 console.log("start to the my new project");
+
+// app.use((req, res) => {
+//   res.send("hello from the server");
+// });
+
+app.use("/home", (req, res) => {
+  res.send("tested successfully");
+});
+
+// app.use("/", (req, res) => {
+//   res.send("home page successfully updated");
+// });
+
+//it is line by line execute it oreder is very very important
+app.get("/user", (req, res) => {
+  res.send("data successfully updated");
+});
+// app.get("/abc", (req, res) => {
+//   res.send("data successfully updated or not ");
+// });
+
+app.get("/abc/:userid", (req, res) => {
+  console.log(req.query);
+  console.log(req.params);
+  res.send("API fetched");
+});
+app.listen(3000, () => {
+  console.log("server started successfully");
+});

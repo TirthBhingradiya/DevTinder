d2c6b6cf60334b72c777dded90b3f0ddcc418c0d
Tirth0003 <tirthbhinagradiya1604@gmail.com>
2025-06-11 23:58:38 +0530

    validation for the signup schema

--- src/models/user.js
+++ src/models/user.js
@@ -4,15 +4,23 @@ const { type } = require("os");
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 50,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required: true,
+    lowerCase: true,
+    unique: true,
+    trim: true,
   },
   password: {
     type: String,
+    required: true,
   },
   age: {
     type: Number,
